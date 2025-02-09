076da949378a50d394adbda3e621854135a4f5f4
sahal2004 <snmansuri8405@gmail.com>
2025-02-09 19:11:29 +0530

    Add files via upload

---
+++ customer/includes/footer.php
@@ -0,0 +1,39 @@
+<footer class="page-footer">
+
+	<div class="footer-nav">
+		<div class="container clearfix">
+
+			
+
+
+			<div class="footer-nav__col footer-nav__col--contacts">
+				<div class="footer-nav__heading">Contact details</div>
+				<address class="address">
+				Head Office: Avenue Fashion.<br>
+				180-182 Regent Street, London.
+			</address>
+				<div class="phone">
+					Telephone:
+					<a class="phone__number" href="tel:0123456789">0123-456-789</a>
+				</div>
+				<div class="email">
+					Email:
+					<a href="mailto:support@yourwebsite.com" class="email__addr">support@yourwebsite.com</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	
+
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; 2025 CloneClothes Fashion&trade;
+			</div>
+
+		</div>
+	</div>
+</footer>
---
+++ customer/pay_offline.php
@@ -0,0 +1,53 @@
+<center><!-- center Starts -->
+
+<h1> Pay OffLine Using Method  </h1>
+
+<p class="text-muted" >
+
+If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+
+</p>
+
+</center><!-- center Ends -->
+
+<hr>
+
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th> Cash </th>
+
+<th> Online</th>
+
+
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!-- tbody Starts -->
+
+<tr>
+
+<td> you have to pay after your clothes deliver </td>
+
+<td>like paytm,google pay,redeem code etc</td>
+
+
+</td>
+
+
+</tr>
+
+</tbody><!-- tbody Ends -->
+
+
+</table><!-- table table-bordered table-hover table-striped Ends -->
+
+</div><!-- table-responsive Ends -->
