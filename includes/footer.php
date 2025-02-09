23588c41cfbc81f18c13b239e438d3305fa29223
sahal2004 <snmansuri8405@gmail.com>
2025-02-09 19:12:29 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<footer class="page-footer">
+
+	<div class="footer-nav">
+		<div class="container clearfix">
+
+			<div class="footer-nav__col footer-nav__col--info">
+			
+
+
+			<div class="footer-nav__col footer-nav__col--contacts">
+				<div class="footer-nav__heading">Contact details</div>
+				<address class="address">
+				Head Office: Clone Clothes.<br>
+            infocity gandhingar, gujarat.
+			</address>
+				<div class="phone">
+					Mobile Number:
+					<a class="phone__number" href="tel:0123456789">6354540406</a>
+				</div>
+				<div class="email">
+					Email:
+					<a href="mailto:support@yourwebsite.com" class="email__addr">support@CloneClothes.com</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; 2025 CloneClothes&trade;
+			</div>
+
+		
+		</div>
+	</div>
+</footer>
